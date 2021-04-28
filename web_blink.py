30cabffd588fd7745ed2e0d079acb17606d96774
Tim Guite <tim@magpieembedded.co.uk>
2021-04-28 19:14:14 +0100

    Added index html which connects to LEDs

--- web_blink.py
+++ web_blink.py
@@ -1,43 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 from gpiozero import LED
 
 app = Flask(__name__)
 
 # Using all the pins next to each other
 # For pinout use: https://pinout.xyz/#
-led_1 = LED(17)
-led_2 = LED(27)
-led_3 = LED(22)
+red_led = LED(17)
+green_led = LED(27)
+blue_led = LED(22)
 
 # Start with LEDs off
-led_1.off()
-led_2.off()
-led_3.off()
+red_led.off()
+green_led.off()
+blue_led.off()
 
 
 @app.route("/")
 def hello_world():
-    return "hello world"
+    return render_template("index.html")
 
 
 @app.route("/led/<int:number>/<value>")
 def control_led(number: int, value: str):
     args = (number, value)
     if args == (1, "on"):
-        led_1.on()
+        red_led.on()
         return "You turned LED 1 On"
     elif args == (1, "off"):
-        led_1.off()
+        red_led.off()
         return "You turned LED 1 Off"
     if args == (2, "on"):
-        led_2.on()
+        green_led.on()
         return "You turned LED 2 On"
     elif args == (2, "off"):
-        led_2.off()
+        green_led.off()
         return "You turned LED 2 Off"
     if args == (3, "on"):
-        led_3.on()
+        blue_led.on()
         return "You turned LED 3 On"
     elif args == (3, "off"):
-        led_3.off()
+        blue_led.off()
         return "You turned LED 3 Off"
